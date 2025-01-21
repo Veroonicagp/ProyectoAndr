121c562b4ef97f46eb85c28f1799e70b7fbd0a28
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2025-01-21 18:47:37 +0100

    h

--- app/src/main/java/com/example/readytoenjoy/ui/activity/ActivitiesListFragment.kt
+++ app/src/main/java/com/example/readytoenjoy/ui/activity/ActivitiesListFragment.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ActivitiesListFragment : Fragment() {
+
     private lateinit var binding: FragmentActivityListBinding
     private val viewModel: ActivityListViewModel by viewModels()
 
